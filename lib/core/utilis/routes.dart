efc3f301159a263743f085950a7451c200577997
Hamdy <hamdey.elattar@gmail.com>
2023-08-11 02:15:43 +0300

    uiDone

--- lib/core/utilis/routes.dart
+++ lib/core/utilis/routes.dart
@@ -1,27 +1,33 @@
 import 'package:books_app/presentation/Screens/book_details_screen/book_detils_screen.dart';
 import 'package:books_app/presentation/Screens/home%20screen/home_screen.dart';
+import 'package:books_app/presentation/Screens/search_screen/search_screen.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../presentation/Screens/splash Screen/splash_view.dart';
 
-abstract class AppRouter
-{
+abstract class AppRouter {
   static const kHomeView = '/homeScreen';
-    static const kBookDetails = '/BookDetails';
- static final router = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const SplashScreen(),
-    ),
-     GoRoute(
-      path: kHomeView,
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
-      path: kBookDetails,
-      builder: (context, state) => const BookDetailsScreen(),
-    ),
-  ],
-);
+  static const kBookDetails = '/BookDetails';
+  static const kSearchScreen = '/searchScreen';
+  
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: kHomeView,
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: kBookDetails,
+        builder: (context, state) => const BookDetailsScreen(),
+      ),
+      GoRoute(
+        path: kSearchScreen,
+        builder: (context, state) => const SearchScreen(),
+      )
+    ],
+  );
 }
